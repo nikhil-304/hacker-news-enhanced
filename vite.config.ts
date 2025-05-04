1eb3adf6df9648396c91fd1df3b09c11c0295c7c
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-05-04 14:06:51 +0530

    build: set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hacker-news-enhanced/",
   server: {
     host: true,
     port: 3000,
